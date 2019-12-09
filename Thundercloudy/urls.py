c2e20dc80e49962a432165f8d418e88655c51d87
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-09 14:54:28 +0900

    [CREATE] Add Form View

--- Thundercloudy/urls.py
+++ Thundercloudy/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('api/', include('api.urls')),
+    path('debug/', include('debug.urls')),
     path('admin/', admin.site.urls),
 ]
--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('session/', views.session, name="login"),
     path('files/', views.get_file_list, name="file_list"),
     path('file/', views.upload, name="upload"),
-    path('file/<str:file_id>/', views.download, name="upload"),
+    path('file/<str:file_id>/', views.download, name="download"),
 ]
