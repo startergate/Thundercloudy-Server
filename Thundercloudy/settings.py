ff576db2ec68489e1ff9a5860b1e60615539a9c2
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-09 11:28:02 +0900

    [CREATE] Media Root

--- Thundercloudy/settings.py
+++ Thundercloudy/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media Root (Folder that gonna be home for your uploaded files)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
 # CSRF Stuff
 
 CSRF_COOKIE_SECURE = False
