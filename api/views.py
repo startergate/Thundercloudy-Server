c4eae017f3cc71795bc97065907ac5c79c7a3ce0
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-09 11:06:13 +0900

    [CREATE] Session View

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
+import json
+
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
@@ -6,6 +9,30 @@ from django.http import HttpResponse, JsonResponse
 
 def index(request):
     return HttpResponse("Hello world")
+
+
+def session(request):
+    if request.method != "POST":
+        return
+
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    res = requests.post('http://sid.donote.co:3000/api/v1/session/', {
+        "type": "login",
+        "clientid": body["clientid"],
+        "userid": body["id"],
+        "password": body["pw"],
+        "isPermanent": True,
+    }).json()
+
+    if res["type"] == "error":
+        return HttpResponse(status=500)
+    return JsonResponse({
+        "sessid": res["response_data"][0],
+        "pid": res["response_data"][1],
+        "nickname": res["response_data"][2],
+        "expire": res["response_data"][3],
+    })
 
 
 def upload(request):
