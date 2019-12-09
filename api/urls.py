0280df7ceb10d732401c07788f2984cec48364a4
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-09 11:04:43 +0900

    [UPDATE] string to str

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name="index"),
+    path('/session', views.session, name="login"),
     path('/files', views.get_file_list, name="file_list"),
     path('/file', views.upload, name="upload"),
-    path('/file/<string:file_id>', views.download, name="upload"),
+    path('/file/<str:file_id>', views.download, name="upload"),
 ]
